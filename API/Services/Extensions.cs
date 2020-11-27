221c8fc847ca2ba28b4347f6a094b52fc9a59b6d
Saahil Claypool <saahil@claypools.org>
2020-11-27 14:05:12 -0500

    Add dotnet interactive notebook

--- API/Services/Extensions.cs
+++ API/Services/Extensions.cs
@@ -1,10 +1,20 @@
+using System;
 using System.Linq;
+using System.Text.Json;
 using BookyApi.API.Db;
 using BookyApi.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookyApi.API.Services.Extensions
 {
+    public static class JsonExtensions
+    {
+        public static void Dbg(this object o) =>
+            Console.WriteLine(JsonSerializer.Serialize(o, new()
+            {
+                WriteIndented = true
+            }));
+    }
     public static class EfExtensions
     {
         public static string TableName<T>(this BookyContext context)
